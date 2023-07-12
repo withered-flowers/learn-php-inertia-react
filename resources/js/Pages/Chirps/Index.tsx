0f6d563c168f1f4b30d8e698dee776b56caee8cc
Withered Flowers <withered-flowers@users.noreply.github.com>
2023-07-12 14:16:48 +0700

    feat: initial commit

---
+++ resources/js/Pages/Chirps/Index.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import ChirpCard from "@/Components/ChirpCard";
+import InputError from "@/Components/InputError";
+import PrimaryButton from "@/Components/PrimaryButton";
+import { useForm, Head } from "@inertiajs/react";
+import { Chirp, PageProps } from "@/types";
+
+type Props = {
+  auth: PageProps["auth"];
+  chirps: Chirp[];
+};
+
+export default function Index({ auth, chirps }: Props) {
+  const { data, setData, post, processing, reset, errors } = useForm({
+    message: "",
+  });
+
+  const submit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    post(route("chirps.store"), { onSuccess: () => reset() });
+  };
+
+  return (
+    <AuthenticatedLayout user={auth.user}>
+      <Head title="Chirps" />
+
+      <div className="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+        <form onSubmit={submit}>
+          <textarea
+            value={data.message}
+            placeholder="What's on your mind?"
+            className="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+            onChange={(e) => setData("message", e.target.value)}
+          ></textarea>
+          <InputError message={errors.message} className="mt-2" />
+          <PrimaryButton className="mt-4" disabled={processing}>
+            Chirp
+          </PrimaryButton>
+        </form>
+
+        <div className="mt-6 bg-white shadow-sm rounded-lg divide-y">
+          {chirps.map((chirp) => (
+            <ChirpCard key={chirp.id} chirp={chirp} />
+          ))}
+        </div>
+      </div>
+    </AuthenticatedLayout>
+  );
+}
---
+++ resources/js/types/index.d.ts
@@ -0,0 +1,22 @@
+export interface User {
+    id: number;
+    name: string;
+    email: string;
+    email_verified_at: string;
+}
+
+export type Chirp = {
+    id: number;
+    message: string;
+    user: Partial<User>;
+    created_at: string;
+    updated_at: string;
+};
+
+export type PageProps<
+    T extends Record<string, unknown> = Record<string, unknown>
+> = T & {
+    auth: {
+        user: User;
+    };
+};
